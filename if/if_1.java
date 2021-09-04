328027b12df8d5f253aab5404ce0a24381da99f9
Kim-wonkyu <dnjsrb7617@naver.com>
2021-09-04 23:08:45 +0900

    Backjoon_if

---
+++ if/if_1.java
@@ -0,0 +1,26 @@
+package if_1;
+
+import java.util.Scanner;
+
+public class if_1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		if (a>b) {
+			System.out.println(">");
+		}
+		else if(a<b) {
+			System.out.println("<");
+		}
+		else if (a==b) {
+			System.out.println("==");
+		}
+	}
+
+}
