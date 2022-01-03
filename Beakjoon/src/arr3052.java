dc99fb0908e3ace0300017c59232896c060221c2
Kim-wonkyu <dnjsrb7617@naver.com>
2022-01-03 23:30:46 +0900

    update arr

---
+++ Beakjoon/src/arr3052.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class arr3052 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		HashSet<Integer> arr = new HashSet<Integer>();
+		
+		for(int i = 0; i < 10; i++) {
+			arr.add(sc.nextInt() % 42);
+		}
+		System.out.println(arr.size());
+	}
+}
