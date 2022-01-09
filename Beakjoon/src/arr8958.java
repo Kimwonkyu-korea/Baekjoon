9d91bd1f9831686cdcfb05d08486ef8bc7d11eed
Kim-wonkyu <dnjsrb7617@naver.com>
2022-01-09 23:47:52 +0900

    update arr

---
+++ Beakjoon/src/arr8958.java
@@ -0,0 +1,7 @@
+public class arr8958 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
--- Beakjoon/src/for15552.java
+++ Beakjoon/src/for15552.java
@@ -1,28 +1,23 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import jdk.internal.org.jline.utils.InputStreamReader;
 
 public class for15552 {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int line = Integer.parseInt(br.readLine());
-
-		StringTokenizer st;
-
-		for(int i = 0; i < line; i++) {
-			st = new StringTokenizer(br.readLine());
-			bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())+"\n");
-		}
-		bw.close();
-		br.close();
+	public static void main(String[] args){
+		 Scanner sc = new Scanner(System.in);
+		 
+	        int count = sc.nextInt();
+	        int a = 0;
+	        int b = 0;
+	        int [] result = new int[count];	// 입력한 값 크기만큼 배열 생성
+	        for(int i=0; i<count; i++) {
+	            a = sc.nextInt();
+	            b = sc.nextInt();
+	            result[i] = a+b;	// 더한 값을 배열에 순서대로 넣어줌
+	        }
+	        
+	        for(int i=0; i<result.length; i++) {
+	            System.out.println(result[i]);	// 출력
+	        }
+	        sc.close();
 	}
 }
