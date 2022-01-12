c3c62b853eee91f0527883d1a3395354581a4fce
Kim-wonkyu <dnjsrb7617@naver.com>
2022-01-12 23:43:00 +0900

    입출력

--- Beakjoon/src/in2588.java
+++ Beakjoon/src/in2588.java
@@ -13,13 +13,14 @@ public class in2588 {
 		int re1 = result1*num1;
 		System.out.println(re1);
 		
-		int result2 = num2/100;
+		int result2 = (num2%100)/10;
 		int re2 = result2*num1;
 		System.out.println(re2);
 		
-		int result3 = (num2%100)/10;
+		int result3 = num2/100;
 		int re3 = result3*num1;
 		System.out.println(re3);
+		
 		
 		System.out.println(re1+(re2*10)+(re3*100));
 	}
